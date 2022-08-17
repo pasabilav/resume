f2885bc276eb560e763e3403086bcbcca2ee63f2
boqpos <boqpos@gmail.com>
2022-08-17 18:02:54 +0200

    Edit

--- scr.js
+++ scr.js
@@ -49,3 +49,29 @@ window.addEventListener('mousemove',function (){
         }
     }
 })
+
+window.addEventListener('mousemove',function (){
+    const circleSection = document.querySelector('.circles');
+    const circles = document.querySelectorAll('.circly');
+    const progressCircles = document.querySelectorAll('.progressbar__svg-circles');
+    const progressText = document.querySelectorAll('.text');
+
+    let bol = false;
+
+    if (pageYOffset>circleSection.offsetTop-600&&bol==false) {
+        for (let i=0;i<circles.length;i++){
+            let radius = progressCircles[i].r.baseVal.value;
+            let circumference = radius * 2 * Math.PI;
+
+            progressCircles[i].style.strokeDasharray=circumference;
+            function setProgress(percent){
+                progressCircles[i].style.strokeDashoffset=
+                    circumference-(percent/100)*circumference;
+            }
+            const progress = circles[i].dataset.prog;
+            // progressText[i].innerHTML=progress+"%";
+            setProgress(progress);
+            bol=true;
+        }
+    }
+})
